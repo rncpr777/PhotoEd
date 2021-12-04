15692f631c9ff6e29d399c3f4ed44e5131a75666
rncpr777 <igoryasha71@gmail.com>
2021-12-04 21:58:53 +0300

    Update file.py

--- file.py
+++ file.py
@@ -1,4 +1,5 @@
 import sys
+import random
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
 
@@ -28,8 +29,13 @@ class Example(QWidget):
         qp.end()
 
     def draw(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
-        qp.drawEllipse(125, 80, 250, 250)
+        c1 = random.randint(0, 255)
+        c2 = random.randint(0, 255)
+        c3 = random.randint(0, 255)
+        x = random.randint(0, 255)
+        y = random.randint(0, 255)
+        qp.setBrush(QColor(c1, c2, c3))
+        qp.drawEllipse((500 - x) // 2, ((500 - x) // 2) - 80, x, y)
 
 
 if __name__ == '__main__':
