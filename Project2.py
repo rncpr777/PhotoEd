b0d0723f464049b9d42d9181196ff56a5d6f2ccf
rncpr777 <95543255+rncpr777@users.noreply.github.com>
2022-06-08 14:27:17 +0300

    Add files via upload

--- Project2.py
+++ Project2.py
@@ -3,10 +3,14 @@ from PIL import Image
 
 print("*********************")
 print("Выберите соотношение:")
-print("1. 16:9")
-print("2. 4:3")
-print("3. 1:1")
+print("1. 16:10")
+print("2. 16:9")
+print("3. 5:4")
+print("4. 4:3")
+print("5. 3:4")
+print("6. 1:1")
 soot = int(input())
+print("******************************************")
 print("Вы хотите сделать изображение черно-белым?")
 chb = bool(input())
 img = Image.open("photo.png")
@@ -15,7 +19,30 @@ width, height = img.size
 if soot == 1:
     delen = width // 16
     width = delen * 16
+    height = delen * 10
+    img = img.resize((width, height))
+elif soot == 2:
+    delen = width // 16
+    width = delen * 16
     height = delen * 9
+    img = img.resize((width, height))
+elif soot == 3:
+    delen = width // 5
+    width = delen * 5
+    height = delen * 4
+    img = img.resize((width, height))
+elif soot == 4:
+    delen = width // 4
+    width = delen * 4
+    height = delen * 3
+    img = img.resize((width, height))
+elif soot == 5:
+    delen = height // 4
+    width = delen * 3
+    height = delen * 4
+    img = img.resize((width, height))
+elif soot == 6:
+    height = width
     img = img.resize((width, height))
 img.save("res.png")
 print("Готово! :)")
